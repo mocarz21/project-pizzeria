2259cbad816cdc7d59be9ccf2c94e303ae6c40aa
Adam Mocarski <adammocarski22@gmail.com>
2022-03-30 19:16:30 +0200

    Add pethod procesOrder

--- src/js/script.js
+++ src/js/script.js
@@ -87,7 +87,10 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.getElements();                                                                                // po co początek thisProductn ?
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
       console.log('new product : ', thisProduct);                                                              //w mozilla kiedy widze na co wskazuje this upewnic sie ze to co jest kolorem  ??
     }
     initAccordion(){
@@ -95,17 +98,19 @@
       
       
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      //const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);              //powtorzyc na co wskazuje this i kiedy !!
       
 
       /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click' , function(event){
+      //clickableTrigger.addEventListener('click', function(event){
+
+      thisProduct.accrdionTrigger.addEventListener('click', function(event){
 
         /* prevent default action for event */
         event.preventDefault();
 
         /* find active product (product that has active class) */
-        let activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);                     // Czy mogę użyć classList po querySelector w celu wyszukania klasy ?
+        let activeProduct = document.querySelector(select.all.menuProductsActive);                     // Czy mogę użyć classList po querySelector w celu wyszukania klasy ?
 
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         if (activeProduct && (thisProduct.element != activeProduct)){                                         //czemu nie działał zapis thisProduct.element != activeProduct ? czemu tak (activeProduct && (thisProduct.element != activeProduct))??
@@ -115,9 +120,65 @@
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
 
-      }); 
-                                                                                                         // po co tenm nawias ????
+      });                                                                                                      // po co tenm nawias ????
+
     }
+    initOrderForm(){
+      const thisProduct = this;
+      console.log('initOrderForm :', this)
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change' , function(){
+          thisProduct.processOrder();
+        });
+      }
+      thisProduct.cartButton.addEventListener('click' , function (event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+    processOrder(){
+      const thisProduct = this;
+
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        
+
+        // for every option in this category
+        for(let optionId in param.options) {
+
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          
+          //sprawdzenie czy jest zaznaczona opcja default: true
+
+          //jezeli nie jest zaznaczona dodaj do price optionId(element prowadzacy do wartosci w obiekcie price) jeżeli nie odejmij od price  optionId(element prowadzacy do wartosci w obiekcie price)
+          
+        }
+      }
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
+
+      
+
+      console.log('price :', price);
+    }
+    
+
     renderInMenu(){
       const thisProduct = this;
       
@@ -135,6 +196,16 @@
 
 
     }
+    getElements(){
+      const thisProduct = this;
+
+      thisProduct.accrdionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      console.log('sprawdzenie przewidywan : ',thisProduct.formInputs)
+    } 
   }
   app.init();
 }
